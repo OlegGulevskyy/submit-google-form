13ef7fb6bcc5d82e4135dc3361ffa3411ccf931c
Oleg Gulevskyy <43781031+OlegGulevskyy@users.noreply.github.com>
2023-01-25 12:14:42 +0100

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/OlegGulevskyy/fillform
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const (
+	PREF_RESP_ID            = "1FAIpQLSe2GMA1Tj1viLoslhmyPILzxN_aqTvKxEHzY-RUrE6_CrPAQQ"
+	PRODUCT_QUESTION_ID     = "entry.491128001"
+	PRODUCT_QUESTION_ANSWER = "ProductA"
+	PRICE_QUESTION_ID       = "entry.558317"
+	PRICE_QUESTION_ANSWER   = "100"
+)
+
+func main() {
+	baseUrl := fmt.Sprint(
+		"https://docs.google.com/forms/d/e/",
+		PREF_RESP_ID,
+		"/formResponse",
+		"?usp=pp_url",
+	)
+	url := fmt.Sprint(
+		baseUrl,
+		"&",
+		PRODUCT_QUESTION_ID,
+		PRODUCT_QUESTION_ANSWER,
+		"&",
+		PRICE_QUESTION_ID,
+		PRICE_QUESTION_ANSWER,
+	)
+
+	resp, err := http.Post(url, "application/x-www-form-urlencoded", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("Response created with status => ", resp.Status)
+}
